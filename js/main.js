44d5df9188fbdb8d1ebd79f79766365f0d8a6daf
manoharprabhu <manoharagent13@gmail.com>
2016-02-19 10:34:46 +0530

    Changed Main page layout

--- js/main.js
+++ js/main.js
@@ -5,10 +5,8 @@
         console.log(data);
         data = JSON.parse(data);
         if (data.src === null) {
-            document.getElementById('placeholder').style.display = 'block';
             document.getElementById('photocaption').style.display = 'none';
         } else {
-            document.getElementById('placeholder').style.display = 'none';
             document.getElementById('photocaption').style.display = 'block';
             document.getElementById('imagetorep').src = 'uploads/' + data.filename;
             document.getElementById('photocaption')
