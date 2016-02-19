cba2e1ad83361c00306b4b122efe851e69760d8f
manoharprabhu <manoharagent13@gmail.com>
2016-02-19 09:31:24 +0530

    Added doctype. Minor bug fixes

--- config.js
+++ config.js
@@ -3,19 +3,19 @@ var config = {
         clientId: '1002595339779964',
         clientSecret: '18742cedfc725c7dada96801457a5c97',
     },
-    hostname: 'nodejs-tracker1990.rhcloud.com',
-    //hostname: 'localhost',
+    //hostname: 'nodejs-tracker1990.rhcloud.com',
+    hostname: 'localhost',
     port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ipAddress: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 }
 
 var isOpenshift = function () {
-    return !(process.env.OPENSHIFT_NODEJS_PORT === undefined)
+    return (typeof process.env.OPENSHIFT_NODEJS_PORT != 'undefined' || process.env.OPENSHIFT_NODEJS_PORT === "")
 }
 
 var getFacebookCallbackUrl = function () {
     //Openshift app runs on 8080, but maps to to port 80
-    if (isOpenshift) {
+    if (isOpenshift()) {
         return "http://" + config.hostname + "/auth/facebook/callback";
     } else {
         return "http://" + config.hostname + ":" + config.port + "/auth/facebook/callback";
