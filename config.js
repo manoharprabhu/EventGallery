00bd85b4d9e0e6aa2fe22094e372619612b0ab04
manoharprabhu <manoharagent13@gmail.com>
2016-02-19 08:26:07 +0530

    Cleanup package.json

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var config = {
         clientId: '1002595339779964',
         clientSecret: '18742cedfc725c7dada96801457a5c97',
     },
-    //hostname: 'nodejs-tracker1990.rhcloud.com',
-    hostname: 'localhost',
+    hostname: 'nodejs-tracker1990.rhcloud.com',
+    //hostname: 'localhost',
     port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ipAddress: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 }
